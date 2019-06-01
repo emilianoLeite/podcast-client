231a0f947677ec8c9f6938a8d63b7783432bdb7b
Lucas Damazio da Costa <lucas.damaziocosta@outlook.com>
2019-06-01 15:34:51 -0300

    Adding PodcastShowScreen, with only basic text.

---
+++ screens/PodcastShowScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function PodcastShowScreen() {
+  return (
+    <View>
+      <Text>
+        PodcastShowScreen
+      </Text>
+    </View>
+  )
+}
