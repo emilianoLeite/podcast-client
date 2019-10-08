0436f5a9df33fc585d6e26be32406f9c3b9486a7
Emiliano Leite <emiliano.leite@codus.com.br>
2019-10-08 14:01:48 -0300

    refactor(styles): Remove prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,25 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    "env": {
+        "browser": true,
+        "es6": true,
+        "react-native/react-native": true
+    },
+    "extends": ["eslint:recommended"],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "react-native"
+    ],
+    "rules": {
+    }
 };
