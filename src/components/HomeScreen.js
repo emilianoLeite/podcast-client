6620ab718eba70f747016baf3057990c0db51c63
Emiliano Leite <emiliano.leite@creditas.com.br>
2020-02-03 20:29:30 -0300

    WIP try to install gesture handler lib. App does not build anymore

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,52 +1,47 @@
-import React from 'react';
-import { parse } from 'react-native-rss-parser';
-import { PlaybackControl } from "./PlaybackControl"
-import { PodcastEpisode } from "./PodcastEpisode"
+import React from "react";
+import { parse } from "react-native-rss-parser";
+import { PlaybackControl } from "./PlaybackControl";
+import { PodcastEpisode } from "./PodcastEpisode";
 
-import {
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
   contentContainer: {
-    paddingTop: 30,
+    paddingTop: 30
   },
   getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
+    alignItems: "center",
+    marginHorizontal: 50
   },
   homeScreenFilename: {
-    marginVertical: 7,
+    marginVertical: 7
   },
   codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
+    backgroundColor: "rgba(0,0,0,0.05)",
     borderRadius: 3,
-    paddingHorizontal: 4,
+    paddingHorizontal: 4
   },
   getStartedText: {
     fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
+    color: "rgba(96,100,109, 1)",
     lineHeight: 24,
-    textAlign: 'center',
+    textAlign: "center"
   },
   helpContainer: {
     marginTop: 15,
-    alignItems: 'center',
+    alignItems: "center"
   },
   helpLink: {
-    paddingVertical: 15,
+    paddingVertical: 15
   },
   helpLinkText: {
     fontSize: 14,
-    color: '#2e78b7',
-  },
+    color: "#2e78b7"
+  }
 });
 
 export default class HomeScreen extends React.Component {
@@ -58,10 +53,10 @@ export default class HomeScreen extends React.Component {
   }
 
   componentDidMount() {
-    fetch('https://rss.simplecast.com/podcasts/282/rss')
-      .then((response) => response.text())
+    fetch("https://rss.simplecast.com/podcasts/282/rss")
+      .then(response => response.text())
       .then(parse)
-      .then((rss) => this.setState({ rss }))
+      .then(rss => this.setState({ rss }))
       .catch(console.error);
     // var path = `${RNFS.DocumentDirectoryPath}podcast_name.mp3`;
     // RNFS.stat(path).then((statResult) => {
@@ -73,7 +68,10 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}
+        >
           <View style={styles.getStartedContainer}>
             <Text style={styles.getStartedText}>Get started by opening</Text>
           </View>
@@ -94,4 +92,56 @@ export default class HomeScreen extends React.Component {
   }
 }
 
+import { /*View, */ TouchableOpacity /*Text*/ } from "react-native";
+import DraggableFlatList from "react-native-draggable-flatlist";
 
+const exampleData = [...Array(20)].map((d, index) => ({
+  key: `item-${index}`, // For example only -- don't use index as your key!
+  label: index,
+  backgroundColor: `rgb(${Math.floor(Math.random() * 255)}, ${index *
+    5}, ${132})`
+}));
+class Example extends Component {
+  state = {
+    data: exampleData
+  };
+
+  renderItem = ({ item, index, drag, isActive }) => {
+    return (
+      <TouchableOpacity
+        style={{
+          height: 100,
+          backgroundColor: isActive ? "blue" : item.backgroundColor,
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+        onLongPress={drag}
+      >
+        <Text
+          style={{
+            fontWeight: "bold",
+            color: "white",
+            fontSize: 32
+          }}
+        >
+          {item.label}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <DraggableFlatList
+          data={this.state.data}
+          renderItem={this.renderItem}
+          keyExtractor={(item, index) => `draggable-item-${item.key}`}
+          onDragEnd={({ data }) => this.setState({ data })}
+        />
+      </View>
+    );
+  }
+}
+
+export default Example;
