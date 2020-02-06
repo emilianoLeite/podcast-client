863a5012f9f581338818af6e62451efc3fedf7fd
Emiliano Leite <emiliano.leite@creditas.com.br>
2020-02-06 19:05:51 -0300

    feat(Subscription): Create Podcast repository.
    Add save to index method.
    Add save to disk method.

--- ROADMAP.md
+++ ROADMAP.md
@@ -2,25 +2,29 @@
 
 ## MVP
 
-- [X] Download a file
+- [x] Download a file
+
   - https://github.com/itinance/react-native-fs
     - https://facebook.github.io/react-native/docs/permissionsandroid
     - https://stackoverflow.com/questions/44546199/how-to-download-a-file-with-react-native
   - https://github.com/react-native-community/async-storage
 
-- [X] Playback a local file
+- [x] Playback a local file
 
-- [X] Playback control
+- [x] Playback control
+
+- [x] Save podcasts by inputing rss
+
+- [ ] List saved podcasts
 
 - [ ] Stream an episode
-
-- [ ] Save podcasts by inputing rss
 
 - [ ] Playback control from notification
 
 - [ ] Compose and rearranje reproduction playlist
 
 - [ ] Jump 10 seconds foward; Jump 10 seconds backwards
+
   - This is necessary because we are not saving episode progress yet
 
 - [ ] Delete local episode
---
+++ src/Repository/Podcast.js
@@ -0,0 +1,66 @@
+const RNFS = require("react-native-fs");
+
+const ROOT_PATH = `${RNFS.DocumentDirectoryPath}/`;
+const INDEX_PATH = `${ROOT_PATH}index.json`;
+
+function readIndex() {
+  return RNFS.readFile(INDEX_PATH, "utf8");
+}
+function allPodcasts() {
+  return readIndex.then(() => {
+    /*....*/
+  });
+}
+
+function saveToDisk(podcastEntity) {
+  const path = `${
+    RNFS.DocumentDirectoryPath
+  }/podcasts/${podcastEntity.getIdentifier()}.json`;
+  RNFS.writeFile(path, JSON.stringify(podcastEntity), "utf8");
+}
+
+function saveToIndex(podcast) {
+  readIndex()
+    .then(result => {
+      console.warn("index", result);
+      return result;
+    })
+    .then(JSON.parse)
+    .then(parsedIndex => {
+      writePodcastToIndex(parsedIndex, podcast);
+    })
+    .catch(error => handleErrors(error, podcast));
+}
+
+function handleErrors(error, podcast) {
+  if (isFileNotExistError(error)) {
+    RNFS.writeFile(INDEX_PATH, JSON.stringify([podcast]), "utf8");
+    console.warn("CRIEI");
+  } else {
+    throw x;
+  }
+}
+
+function isFileNotExistError(error) {
+  const noFileErrorMessage = RegExp("No such file or directory", "ig");
+  return noFileErrorMessage.test(error.message);
+}
+
+function writePodcastToIndex(index, podcast) {
+  console.warn(index.length);
+
+  const foundPodcast = index.find(indexItem => {
+    return indexItem.identifier === podcast.getIdentifier();
+  });
+
+  if (!foundPodcast) {
+    RNFS.writeFile(INDEX_PATH, JSON.stringify([...index, podcast]), "utf8");
+  }
+}
+
+export const PodcastRepository = {
+  addPodcast(podcast) {
+    saveToDisk(podcast);
+    saveToIndex(podcast);
+  }
+};
