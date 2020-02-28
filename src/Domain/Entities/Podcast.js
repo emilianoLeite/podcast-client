d6fbb7ad87f9b581fbcb0f4bcc9c8d474c62914a
Emiliano Leite <emiliano.leite@creditas.com.br>
2020-02-28 10:18:53 -0300

    feat(PodcastList): Show title instead of identifier

--- src/Domain/Entities/Podcast.js
+++ src/Domain/Entities/Podcast.js
@@ -1,9 +1,14 @@
+// @ts-check
 export class Podcast {
   constructor(podcast) {
     this.identifier = podcast.identifier;
+    this.title = podcast.title;
   }
 
   getIdentifier() {
     return this.identifier;
+  }
+  getTitle() {
+    return this.title;
   }
 }
