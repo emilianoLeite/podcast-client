60724e0ce9ad45f996baae6aff36b0590cd575b6
Emiliano Leite <emiliano.leite@creditas.com.br>
2020-02-28 10:20:22 -0300

    fix(PodcastInput): Correctly instantiate podcast entity before saving

--- src/Domain/Factories/PodcastFactory.js
+++ src/Domain/Factories/PodcastFactory.js
@@ -1,7 +1,7 @@
 import { Podcast } from "../Entities/Podcast";
 
-function create(podcastItem) {
-  return new Podcast(podcastItem);
+function create(mappedPodcastItem) {
+  return new Podcast(mappedPodcastItem);
 }
 
 export default {
